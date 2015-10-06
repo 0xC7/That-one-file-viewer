aa86a76ca72ee6de43abc62e5941e1e57571079a
Xenhat Liamano <xenhat@polarityviewer.org>
2015-10-06 17:41:16 -0400

    added: colors!

--- sharex_control.sh
+++ sharex_control.sh
@@ -8,7 +8,7 @@ export sharex_root="${0%/*}"
 SHAREX_SESSION_NAME="sharex_server.${USER}"
 start(){
 	if [ "$(status)" = "Running" ]; then
-		echo "[Error] Ruby file viewer already running!"
+		echo -e "[\e[1;31mError\e[0m] Ruby file viewer already running!"
 	else
 		screen -dmS "${SHAREX_SESSION_NAME}" bash
 		if [ ! $(command -v ruby >/dev/null 2>&1) ] ; then
@@ -17,7 +17,7 @@ start(){
 		fi
 		# attempt #2
 		if [ $(command -v ruby >/dev/null 2>&1) ]; then
-			echo "[Error] Unable to find ruby executable. Server \e[1;31mNOT\e[0m started."
+			echo -e "[\e[1;31mError\e[0m] Unable to find ruby executable. Server \e[1;31mNOT\e[0m started."
 		else
 			screen -S "${SHAREX_SESSION_NAME}" -X stuff 'cd ${sharex_root}'$(echo -ne '\015')
 			screen -S "${SHAREX_SESSION_NAME}" -X stuff 'ruby sharex_server.rb'$(echo -ne '\015')
@@ -30,7 +30,7 @@ stop(){
 		screen -X -S "${SHAREX_SESSION_NAME}" quit
 		echo "Ruby file viewer stopped."
 	else
-		echo "[Error] Ruby file viewer not running!"
+		echo -e "[\e[1;31mError\e[0m] Ruby file viewer not running!"
 	fi
 }
 restart(){
@@ -60,7 +60,7 @@ case "$1" in
 		if [ "$(status)" = "Running" ]; then
 			screen -dr "${SHAREX_SESSION_NAME}"
 		else
-			echo "[Error] Ruby file viewer not running!"
+			echo -e "[\e[1;31mError\e[0m] Ruby file viewer not running!"
 		fi
 		;;
 	status)
